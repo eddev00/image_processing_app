b906a38d1d9190034a6a8a76cd9605eeec109cd1
Drk <eddevjo@gmail.com>
2024-07-17 21:27:42 +0100

    fix images issue domain

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {images: {
-    domains: ['localhost'],
+    domains: ['localhost','backend-imageprocessing.onrender.com'],
   },};
 
 export default nextConfig;
