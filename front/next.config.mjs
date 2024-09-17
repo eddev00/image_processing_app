80d8e78693bc091407ea26d6a8f961230eafc946
Drk <eddevjo@gmail.com>
2024-09-18 00:51:56 +0100

    img config

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {images: {
-    domains: ['localhost','image-processing-webapp-ggdfwh855-eddev00s-projects.vercel.app'],
+    domains: ['localhost','image-processing-webapp-ggdfwh855-eddev00s-projects.vercel.app','backend-imageprocessing.onrender.com'],
   },};
 
 export default nextConfig;
